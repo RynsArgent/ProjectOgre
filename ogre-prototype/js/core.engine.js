be6afbe9d3468d99484c4b3f4783f1e09f1746ae
Kevin Phung <phung.kevin@gmail.com>
2013-07-16 20:48:45 -0700

    design notes

---
+++ ogre-prototype/js/core.engine.js
@@ -0,0 +1,24 @@
+var CoreEngine = module.exports = function()
+{
+	this.Terrain = {
+		'p' : {label: 'Plains', imagesrc: 'images/Plains.png'},
+		'w' : {label: 'Sea', imagesrc: 'images/Seas.png'},
+		'm' : {label: 'Mountains', imagesrc: 'images/Mountains.png'},
+		'f' : {label: 'Forests', imagesrc: 'images/Forests.png'},
+		's' : {label: 'Swamps', imagesrc: 'images/Swamps.png'},
+	
+		'df' : {label: 'dforest', imagesrc: 'images/ForestDeserts.png'},
+		'dm' : {label: 'dmountain', imagesrc: 'images/MountainDeserts.png'},
+		'dn' : {label: 'desert', imagesrc: 'images/Deserts.png'},
+		'pf' : {label: 'pforest', imagesrc: 'images/ForestPlains.png'},
+		'pm' : {label: 'pmountain', imagesrc: 'images/MountainPlains.png'},
+		'pn' : {label: 'plains', imagesrc: 'images/Plains.png'},
+		'sf' : {label: 'sforest', imagesrc: 'images/ForestSnows.png'},
+		'sm' : {label: 'smountain', imagesrc: 'images/MountainSnows.png'},
+		'sn' : {label: 'snow', imagesrc: 'images/Snows.png'},
+		'wb' : {label: 'beach', imagesrc: 'images/Beaches.png'},
+		'wo' : {label: 'ocean', imagesrc: 'images/Oceans.png'},
+		'ws' : {label: 'sea', imagesrc: 'images/Seas.png'},
+	};
+}
+
