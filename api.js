a781dafd240728bacf8f1c145a51ba6be39e57de
tesract <tesract@gmail.com>
2013-10-09 13:10:36 -0700

    changed api to use dependancy injection

--- api.js
+++ api.js
@@ -1,43 +1,41 @@
 
-var modeler = require('modeler-redis')
- 	, client = require('redis').createClient()
-	, idgen = require('idgen')
-	, bcrypt = require('bcrypt')
-;
+var	  bcrypt = require('bcrypt');
 
-// entity model
-var user = modeler({
-	client: client // must pass a node_redis client here
-	,name: 'user' // keys will be prefixed by the collection name
-	,prefix: 'OGRE_' // optional root prefix for keys
-});
-
-module.exports.api = {
-	createUser: function(data, cb) {
-		bcrypt.hash(data.pass, 5, function( err, cPass) {
-			var me = user.create({
-				id:data.email
-				,pass:cPass
-				,alias:data.alias
-			});
-
-			user.save(me, function(err){
-				cb(me);
-			});
-		});
-	}
-
-	, getUser: function(data, cb) {
-		user.load(data.email, function(err, me){
-			bcrypt.compare(data.pass, me.pass, function(err, doesMatch){
-				if (doesMatch){
-					cb(me);
-				}else{
-					cb('bad pw');
-				}
-			 });
-		});
-	}
+module.exports.api = function(modeler, client)
+{
+	// entity model
+	this.user = modeler({
+		client: client // must pass a node_redis client here
+		,name: 'user' // keys will be prefixed by the collection name
+		,prefix: 'OGRE_' // optional root prefix for keys
+	});	
 };
 
+module.exports.api.prototype.createUser = function(data, cb) 
+{
+	var self = this;
+	bcrypt.hash(data.pass, 5, function( err, cPass) {
+		var me = self.user.create({
+			id:data.email
+			,pass:cPass
+			,alias:data.alias
+		});
 
+		self.user.save(me, function(err){
+			cb(me);
+		});
+	});
+};
+
+module.exports.api.prototype.getUser= function(data, cb) 
+{
+	this.user.load(data.email, function(err, me){
+		bcrypt.compare(data.pass, me.pass, function(err, doesMatch){
+			if (doesMatch){
+				cb(me);
+			}else{
+				cb('bad pw');
+			}
+		 });
+	});
+};
